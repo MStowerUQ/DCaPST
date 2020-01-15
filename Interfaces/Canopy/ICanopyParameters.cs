db9fb731406959aae39421539a195499097fcc4b
Michael Stower <m.stower@uq.edu.au>
2020-01-16 09:57:02 +1000

    Moved interfaces

--- Interfaces/Canopy/ICanopyParameters.cs
+++ Interfaces/Canopy/ICanopyParameters.cs
@@ -2,6 +2,8 @@
 {
     public interface ICanopyParameters
     {
+        IPathwayParameters Path { get; set; }
+
         double Ca { get; set; }
         double U0 { get; set; }
         double LeafAngle { get; set; }
@@ -13,15 +15,11 @@
         double DiffuseReflectionCoeffNIR { get; set; }
         double LeafWidth { get; set; }
         double Ku { get; set; }
-        double Vpr_l { get; set; }
-        double Theta { get; set; }
-        double F { get; set; }
-        double Alpha { get; set; }
-        double Constant { get; set; }
-        double Gbs_CO2 { get; set; }
-        double Sigma { get; set; }
-        double Rcp { get; set; }
-        double PsychrometricConstant { get; set; }
-        double Lambda { get; set; }
+        double EmpiricalSpectralCorrectionFactor { get; set; }        
+        double DiffusivitySolubilityRatio { get; set; }
+
+        double StructuralN { get; set; }
+        double SLNRatioTop { get; set; }
+        double SLNAv { get; set; }
     }
 }
--- Interfaces/Canopy/IPathwayParameters.cs
+++ Interfaces/Canopy/IPathwayParameters.cs
@@ -3,16 +3,13 @@
 namespace DCAPST.Interfaces
 {
     public interface IPathwayParameters
-    {
-        ICanopyParameters Canopy { get; set; }
-
-        double StructuralN { get; set; }
-        double SLNRatioTop { get; set; }
-        double SLNAv { get; set; }
+    {       
+        double ConvexityFactor { get; set; }
+        double Alpha { get; set; }
+        double Vpr_l { get; set; }
+        double Gbs_CO2 { get; set; }
 
         double CiCaRatio { get; set; }
-        double CiCaRatioIntercept { get; set; }
-        double CiCaRatioSlope { get; set; }
         double Fcyc { get; set; }
         double PsiRd { get; set; }
         double PsiVc { get; set; }
@@ -22,12 +19,7 @@ namespace DCAPST.Interfaces
         double X { get; set; }
         double z { get; set; }
 
-        // KineticParams       
-        double F2 { get; set; }
-        double F1 { get; set; }
         double Phi { get; set; }
-
-        // Curvilinear Temperature Model
         double KcP25 { get; set; }
         double KcTEa { get; set; }
         double KoP25 { get; set; }
@@ -51,7 +43,5 @@ namespace DCAPST.Interfaces
         double GmBeta { get; set; }
 
         AssimilationParameters GetAssimilationParams(PartialCanopy canopy);
-
-        //double CalculateAssimilation(AssimilationParameters s);
     }
 }
