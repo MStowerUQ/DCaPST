010501a84e18fa03f8470baf282beab79969237e
Michael Stower <m.stower@uq.edu.au>
2020-01-16 09:57:17 +1000

    Create IRadiation.cs

---
+++ Interfaces/Environment/IRadiation.cs
@@ -0,0 +1,14 @@
+﻿namespace DCAPST.Interfaces
+{
+    public interface IRadiation
+    {
+        double TotalIncidentRadiation { get; }
+        double DirectRadiation { get; }
+        double DiffuseRadiation { get; }
+        double DirectRadiationPAR { get; }
+        double DiffuseRadiationPAR { get; }
+
+
+        void UpdateHourlyRadiation(double time);
+    }
+}
